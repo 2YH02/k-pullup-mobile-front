34866775c4199c721a94e222c22f4444ebb7d357
2YH02 <yonghuni484@gmail.com>
2025-03-14 20:30:51 +0900

    Feat: 커스텀 오버레이 스타일 변경

--- src/components/overlay/overlay.tsx
+++ src/components/overlay/overlay.tsx
@@ -1,5 +1,6 @@
 import { useMapStore } from "@/store/use-map-store";
 import { Pos } from "@/types/kakao-map.types";
+import cn from "@/utils/cn";
 
 interface OverlayProps {
   title: string;
@@ -18,12 +19,41 @@ const Overlay = ({ title, position }: OverlayProps) => {
   };
 
   return (
-    <div className="bg-white dark:bg-black absolute -bottom-3 -left-10 w-[80px] h-[55px] flex items-center justify-center rounded-[3rem]">
-      <button className="w-full h-full" onClick={handleClick}>
+    <div
+      className={cn(
+        "opacity-80 absolut -bottom-3 -left-10 w-[60px] h-[60px] flex items-center justify-center rounded-full shadow-full",
+        getTailwindColorClass(convertToNumber(title))
+      )}
+    >
+      <button className="w-full h-full z-10 text-white" onClick={handleClick}>
         <div>{title}</div>
       </button>
+      <span
+        className={cn(
+          "absolute inline-flex h-full w-full animate-ping rounded-full opacity-65",
+          getTailwindColorClass(convertToNumber(title))
+        )}
+      ></span>
     </div>
   );
+};
+
+const getTailwindColorClass = (count: number): string => {
+  if (count < 100) {
+    return "bg-green";
+  } else if (count < 500) {
+    return "bg-blue";
+  } else if (count < 1000) {
+    return "bg-yellow";
+  } else if (count < 5000) {
+    return "bg-red";
+  } else {
+    return "bg-red";
+  }
+};
+
+const convertToNumber = (str: string): number => {
+  return parseInt(str, 10);
 };
 
 export default Overlay;
